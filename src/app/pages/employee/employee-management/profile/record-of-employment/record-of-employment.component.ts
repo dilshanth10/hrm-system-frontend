423650b84cdd08d496be438cbe239d240bace322
Priya <priyavisuvalingam94@gmail.com>
2018-11-02 17:58:39 +0530

    record of employment component

--- src/app/pages/employee/employee-management/profile/record-of-employment/record-of-employment.component.ts
+++ src/app/pages/employee/employee-management/profile/record-of-employment/record-of-employment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-record-of-employment',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordOfEmploymentComponent implements OnInit {
 
+  displayedColumns: string[] = ['role', 'status','period','name','leave','salary'];
+
+  role = [
+    {  'roleName':'', 'status': '','period':'','name':'','leave':'','salary':''},
+    {  'roleName':'', 'status': '','period':'','name':'','leave':'','salary':''},
+    {  'roleName':'', 'status': '','period':'','name':'','leave':'','salary':''}
+   
+    
+    
+  ]
+  dataSource = new MatTableDataSource<any>(this.role);
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor() { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<any>(this.role);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
