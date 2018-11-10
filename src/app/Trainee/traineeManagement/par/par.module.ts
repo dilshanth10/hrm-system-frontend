84fc424104142fb31efcfc6f9f7fff780ecaa201
Thanushan <43714629+ThanushanR@users.noreply.github.com>
2018-11-10 12:18:33 +0530

    par History done

--- src/app/Trainee/traineeManagement/par/par.module.ts
+++ src/app/Trainee/traineeManagement/par/par.module.ts
@@ -5,6 +5,10 @@ import { ParScheduleComponent } from './par-schedule/par-schedule.component';
 import { ParHistoryComponent } from './par-history/par-history.component';
 
 import {RouterModule,Routes} from '@angular/router';
+
+
+import { FormsModule } from '@angular/forms';
+import { MaterialModuleModule } from 'src/app/material-module.module';
 const routes:Routes=[
 {
   path:'',
@@ -14,6 +18,8 @@ const routes:Routes=[
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModuleModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [ParComponent, ParScheduleComponent, ParHistoryComponent]
