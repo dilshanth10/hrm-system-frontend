14f69f25631dad6d006a426244ddc8bf6447399b
Inthuja <inthujae@gmail.com>
2018-11-10 12:05:43 +0530

    Project & posting folder structure

--- src/app/Trainee/posting/posting.module.ts
+++ src/app/Trainee/posting/posting.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostingComponent } from './posting.component';
+import {RouterModule,Routes} from '@angular/router';
+import { RecordPostingDetailsComponent } from './record-posting-details/record-posting-details.component';
 
+
+const routes:Routes=[
+  {
+    path:'',
+   component:PostingComponent
+  }
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: []
+  declarations: [PostingComponent, RecordPostingDetailsComponent]
 })
 export class PostingModule { }
