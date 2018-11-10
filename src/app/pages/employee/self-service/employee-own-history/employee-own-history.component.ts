3b841b403f37b3cdc83011e4d0539d253aa5c0c2
ragulan22 <balasingamragulan@gmail.com>
2018-11-10 09:16:57 +0530

    individual complain,e,ploye complain history modification

--- src/app/pages/employee/self-service/employee-own-history/employee-own-history.component.ts
+++ src/app/pages/employee/self-service/employee-own-history/employee-own-history.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+
 
 @Component({
   selector: 'app-employee-own-history',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeOwnHistoryComponent implements OnInit {
 
+  displayedColumns: string[] = ['date', 'type','status','description','attach','reply','myReply','sendReply'];
+
+  history = [
+    { 'date':'1', 'type':'Manager', 'status':'12/1/2018', 'description' :'attach', 'attach':'wrfwr', 'reply':'jhbj','myReply':'vwwvw'},
+    { 'date':'2', 'type':'Manager', 'status':'12/1/2018', 'description' :'attach', 'attach':'wrfwr', 'reply':'jhbj','myReply':'vwwvw'}
+ 
+  ]
+  dataSource = new MatTableDataSource<any>(this.history);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor() { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<any>(this.history);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
