5f2711e19a9ae26fc085c3f3ceb63c8a48b0170d
Daminiya <daminiyajeya@gmail.com>
2018-11-10 17:28:46 +0530

    Posting preparence

--- src/app/Trainee/posting/posting.module.ts
+++ src/app/Trainee/posting/posting.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { PostingComponent } from './posting.component';
 import {RouterModule,Routes} from '@angular/router';
 import { RecordPostingDetailsComponent } from './record-posting-details/record-posting-details.component';
+import { RecordPostingPreferenceComponent } from './record-posting-preference/record-posting-preference.component';
+import { ViewRecordPostingPreferenceComponent } from './view-record-posting-preference/view-record-posting-preference.component';
+import { MaterialModuleModule } from 'src/app/material-module.module';
 
 
 const routes:Routes=[
@@ -14,8 +17,9 @@ const routes:Routes=[
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModuleModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [PostingComponent, RecordPostingDetailsComponent]
+  declarations: [PostingComponent, RecordPostingDetailsComponent, RecordPostingPreferenceComponent, ViewRecordPostingPreferenceComponent]
 })
 export class PostingModule { }
