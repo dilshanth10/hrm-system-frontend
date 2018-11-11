b6ad6de145fd85cc98a3b5a0f1e12abe7aba2be6
Dilshanth <dilshanth10@gmail.com>
2018-11-12 00:35:15 +0530

    finalized folder structure

---
+++ src/app/modules/employee/employee-management/experience-letter/experience-letter.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExperienceLetterComponent } from './experience-letter.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ExperienceLetterComponent]
+})
+export class ExperienceLetterModule { }
---
+++ src/app/modules/employee/leave-management/leave-management.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LeaveManagementComponent } from './leave-management.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LeaveManagementComponent]
+})
+export class LeaveManagementModule { }
