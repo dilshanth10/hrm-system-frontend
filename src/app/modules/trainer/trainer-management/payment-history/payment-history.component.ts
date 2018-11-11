6ea74d6f6048407b90914d962540c021c6c81aca
sujaany <ssujaany09@gmail.com>
2018-11-11 10:05:46 +0530

    payment history

--- src/app/modules/trainer/trainer-management/payment-history/payment-history.component.ts
+++ src/app/modules/trainer/trainer-management/payment-history/payment-history.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-payment-history',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-history.component.css']
 })
 export class PaymentHistoryComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name','date','amount','status','update/delete'];
+
+  paymentHistry = [
+    { 'id':'1', 'name':'John', 'date' :'17.11.2018','amount':'25000.00','status':'Paid','update/delete':'' },
+    { 'id':'2', 'name':'Raja', 'date' :'18.11.2018','amount':'25000.00','status':'Unpaid','update/delete':'' },
+  
+    
+  ]
+  dataSource = new MatTableDataSource<any>(this.paymentHistry);
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<any>(this.paymentHistry);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
+
