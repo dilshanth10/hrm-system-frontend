3face3e407c4e11b43870065022948d2e41e9797
Priya <priyavisuvalingam94@gmail.com>
2018-11-14 13:54:21 +0530

    leave Management routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
+
 const routes: Routes = [
 
   // general
@@ -20,6 +21,10 @@ const routes: Routes = [
   {
     path: 'recruitment',
     loadChildren: './modules/employee/recruitment/recruitment.module#RecruitmentModule'
+  },
+  {
+    path: 'leaveManagement',
+    loadChildren: './modules/employee/leave-management/leave-management.module#LeaveManagementModule'
   },
   {
     path: 'welfare',
--- src/app/modules/employee/leave-management/leave-management.module.ts
+++ src/app/modules/employee/leave-management/leave-management.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeaveManagementComponent } from './leave-management.component';
+import { CarryForwardLeaveRequestComponent } from './carry-forward-leave-request/carry-forward-leave-request.component';
+import { CarryForwardLeaveComponent } from './carry-forward-leave/carry-forward-leave.component';
+import {RouterModule,Routes} from '@angular/router';
+import { MaterialModuleModule } from 'src/app/material-module.module';
+
+const routes:Routes=[
+  {
+    path:'',
+    component:LeaveManagementComponent,
+
+  }
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModuleModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [LeaveManagementComponent]
+  declarations: [LeaveManagementComponent, CarryForwardLeaveRequestComponent, CarryForwardLeaveComponent]
 })
 export class LeaveManagementModule { }
