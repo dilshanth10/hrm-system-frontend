27b60ac223c0d9edc3d9e6f7c18a52f8c1cb07b7
Jeyarni <jeya08sivam@gmail.com>
2018-11-15 10:21:21 +0530

    Edit Final Assesment

--- src/app/modules/trainee/traineeManagement/finalassesment/view-final-assesment-score/view-final-assesment-score.component.ts
+++ src/app/modules/trainee/traineeManagement/finalassesment/view-final-assesment-score/view-final-assesment-score.component.ts
@@ -12,16 +12,15 @@ export class ViewFinalAssesmentScoreComponent implements OnInit {
   displayedColumns: string[] = ['traineenum','traineename', 'matricesmeasured','relevantscromatrics','finalscore','remarks','edit/delete'];
 
   finalassesment = [
-    { 'traineenum':'1', 'traineename':'aa' ,'matricesmeasured':'AA1','relevantscromatrics':'q','finalscore':'3','remarks':'3','edit/delete':''},
-    { 'traineenum':'2', 'traineename':'bb' ,'matricesmeasured':'BB1','relevantscromatrics':'w','finalscore':'6','remarks':'3','edit/delete':''},
-    { 'traineenum':'3', 'traineename':'cc' ,'matricesmeasured':'CC1','relevantscromatrics':'e','finalscore':'2','remarks':'3','edit/delete':''},
-    { 'traineenum':'4', 'traineename':'dd' ,'matricesmeasured':'DD1','relevantscromatrics':'r','finalscore':'5','remarks':'3','edit/delete':''},
-    { 'traineenum':'5', 'traineename':'ee' ,'matricesmeasured':'EE1','relevantscromatrics':'t','finalscore':'4','remarks':'3','edit/delete':''}
+    { 'traineenum':'1', 'traineename':'aa' ,'matricesmeasured':'m1','relevantscromatrics':'s1','finalscore':'3','remarks':'r1','edit/delete':''},
+    { 'traineenum':'2', 'traineename':'bb' ,'matricesmeasured':'m2','relevantscromatrics':'s2','finalscore':'6','remarks':'r2','edit/delete':''},
+    { 'traineenum':'3', 'traineename':'cc' ,'matricesmeasured':'m3','relevantscromatrics':'s3','finalscore':'2','remarks':'r3','edit/delete':''},
+    { 'traineenum':'4', 'traineename':'dd' ,'matricesmeasured':'m4','relevantscromatrics':'s4','finalscore':'5','remarks':'r4','edit/delete':''},
+    { 'traineenum':'5', 'traineename':'ee' ,'matricesmeasured':'m5','relevantscromatrics':'s5','finalscore':'4','remarks':'r5','edit/delete':''}
     
   ]
+  
   dataSource = new MatTableDataSource<any>(this.finalassesment);
-
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
@@ -31,15 +30,6 @@ export class ViewFinalAssesmentScoreComponent implements OnInit {
     this.dataSource = new MatTableDataSource<any>(this.finalassesment);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+
   }
-
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-
-
 }
