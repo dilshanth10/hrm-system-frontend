b064e892774369b6c0f05854cd38dc3db6e4ede1
Jeyarni <jeya08sivam@gmail.com>
2018-11-16 17:06:40 +0530

    promotion

--- src/app/modules/employee/employee-management/promotion/promotion-history/promotion-history.component.ts
+++ src/app/modules/employee/employee-management/promotion/promotion-history/promotion-history.component.ts
@@ -7,11 +7,11 @@ import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
   styleUrls: ['./promotion-history.component.css']
 })
 export class PromotionHistoryComponent implements OnInit {
-  displayedColumns: string[] = ['emploid', 'emploname', 'DofJoin', 'position','promdate','promremark','promsalary','edit/delete'];
+  displayedColumns: string[] = ['emploid', 'emploname',  'position','promdate','promremark','promsalary','edit/delete'];
 
   Creditcheck = [
-    { 'emploid':'001', 'emploname':'em1', 'DofJoin':'25.11.2000', 'position':'ASE','promdate':'25.04.2001','promremark':'Good','promsalary':'80000','edit/delete':'' },
-    { 'emploid':'002', 'emploname':'em2', 'DofJoin':'25.11.2005', 'position':'PM','promdate':'25.04.2006','promremark':'Good','promsalary':'90000','edit/delete':'' }
+    { 'emploid':'001', 'emploname':'em1',  'position':'ASE','promdate':'25.04.2001','promremark':'Good','promsalary':'80000','edit/delete':'' },
+    { 'emploid':'002', 'emploname':'em2',  'position':'PM','promdate':'25.04.2006','promremark':'Good','promsalary':'90000','edit/delete':'' }
   
   ]
   dataSource = new MatTableDataSource<any>(this.Creditcheck);
