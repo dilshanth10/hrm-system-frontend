78bb0eca3074a6bdf993b49eb2059a4d00a7c195
PasinduSandakelum <tmpsthilakarathna2017@gmail.com>
2018-11-20 16:38:15 +0530

    routing for score assignments

--- src/app/modules/trainee/assignments/assignments.module.ts
+++ src/app/modules/trainee/assignments/assignments.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScoreAssignmentsComponent } from './score-assignments/score-assignments.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+   path: 'score-assignments', component: ScoreAssignmentsComponent
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
   ],
   declarations: [ScoreAssignmentsComponent]
 })
