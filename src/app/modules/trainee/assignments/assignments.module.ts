6ab0b639132ffd41f3dd3787357cad382b0ff6f3
PasinduSandakelum <tmpsthilakarathna2017@gmail.com>
2018-11-20 16:46:15 +0530

    change on score-assignments

--- src/app/modules/trainee/assignments/assignments.module.ts
+++ src/app/modules/trainee/assignments/assignments.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScoreAssignmentsComponent } from './score-assignments/score-assignments.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialModuleModule } from 'src/app/material-module.module';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModuleModule,
     RouterModule.forChild(routes),
   ],
   declarations: [ScoreAssignmentsComponent]
