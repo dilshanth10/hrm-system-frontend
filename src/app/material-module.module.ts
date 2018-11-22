17e0fb12a4f7b2aa6d210d3ce261c3fa65178ead
JeyapriyaJegatheeswaran <jjypriya@gmail.com>
2018-11-22 20:24:40 +0530

    request termination integration

--- src/app/material-module.module.ts
+++ src/app/material-module.module.ts
@@ -36,7 +36,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
   ],
   exports: [
-    ReactiveFormsModule,
+    //ReactiveFormsModule,
     MatTableModule,
     MatButtonModule,
     MatFormFieldModule,
