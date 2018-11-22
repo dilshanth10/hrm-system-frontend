e59f602c421688fac846c48ab212f5b4b0b63d3c
sujaany <ssujaany09@gmail.com>
2018-11-22 19:32:32 +0530

    Career Development Plan Intergration Model & Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MaterialModuleModule } from './material-module.module';
 import { HttpClientModule } from '@angular/common/http';
+import { CareerDevPlan } from './modules/general/career-dev-plan/Model/career-dev-plan';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [CareerDevPlan],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
