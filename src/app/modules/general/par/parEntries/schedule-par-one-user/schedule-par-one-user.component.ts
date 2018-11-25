ca9fea89734d00e0f40a46dff454dca962c99fb2
Thiruparan <thirupparan1994@gmail.com>
2018-11-25 18:24:25 +0530

    some service added

--- src/app/modules/general/par/parEntries/schedule-par-one-user/schedule-par-one-user.component.ts
+++ src/app/modules/general/par/parEntries/schedule-par-one-user/schedule-par-one-user.component.ts
@@ -5,6 +5,9 @@ import { ParConfig } from '../../models/par-config.model';
 import { SchedulePar } from '../../models/schedule-par.model';
 import { ScheduleParAppraisors } from '../../models/schedule-par-appraisors.model';
 import { ScheduleParContent } from '../../models/schedule-par-content.model';
+import { ParAppraisorService } from '../../services/par-appraisor.service';
+import { ParAppraisor } from '../../models/par-appraisor.model';
+import { ScheduleParService } from '../../services/schedule-par.service';
 
 @Component({
   selector: 'app-schedule-par-one-user',
@@ -13,68 +16,81 @@ import { ScheduleParContent } from '../../models/schedule-par-content.model';
 })
 export class ScheduleParOneUserComponent implements OnInit {
 
-  scheduleParObj:SchedulePar=new SchedulePar();
-  objApp1:ScheduleParAppraisors=new ScheduleParAppraisors();
-  objApp2:ScheduleParAppraisors=new ScheduleParAppraisors();
+  scheduleParObj: SchedulePar = new SchedulePar();
+  parConfigArray: ParConfig[];
+  parAppraisorArray: ParAppraisor[];
 
-  objCon1:ScheduleParContent=new ScheduleParContent();
-  objCon2:ScheduleParContent=new ScheduleParContent();
-  parConfigArray:ParConfig[];
+  // objApp1:ScheduleParAppraisors=new ;
+  // objApp2:ScheduleParAppraisors=new ScheduleParAppraisors();
 
-  firstFormGroup: FormGroup=new FormGroup({
-    parId:new FormControl(),
-    empId:new FormControl(),
-    parDate:new FormControl(),
-    parTime:new FormControl(),
-    checklist:new FormArray([
 
-    ])
+  firstFormGroup: FormGroup = new FormGroup({
+    parId: new FormControl(),
+    empId: new FormControl(),
+    parDate: new FormControl(),
+    parTime: new FormControl(),
+
   });
-  secondFormGroup: FormGroup;
-
-  constructor(private parConfigService:ParconfigService) {}
 
 
+  constructor(
+    private parConfigService: ParconfigService,
+    private parAppraisorService: ParAppraisorService,
+    private scheduleParService:ScheduleParService
+  ) { }
 
- 
+  getAppraisorsdata() {
+    this.parAppraisorService.getParAppraisor().subscribe(data => {
+      console.log(data);
+      this.parAppraisorArray = data;
+    })
+  }
+
+
 
   ngOnInit() {
 
-    this.parConfigService.getParConfig().subscribe(data=>{
-      this.parConfigArray=data;
+    this.parConfigService.getParConfig().subscribe(data => {
+      this.parConfigArray = data;
     })
-  
+    this.getAppraisorsdata();
   }
 
-  getFormData(){
-    this.scheduleParObj.id=this.firstFormGroup.value.parId;
-    this.scheduleParObj.empId=this.firstFormGroup.value.empId;
-    this.scheduleParObj.scheduleDate=new Date(this.firstFormGroup.value.parDate);
-
-
-    this.objApp1.appraisorId="A001";
-    this.scheduleParObj.scheduleParAppraisorsList.push(this.objApp1);
-
-  
-   
-    this.objApp2.appraisorId="A002";
-    this.scheduleParObj.scheduleParAppraisorsList.push(this.objApp2);
-
-   
-    this.objCon1.parContentId="C001";
-   
-
-    this.scheduleParObj.scheduleParContentList.push(this.objCon1);
-
-    
-  
-    this.objCon2.parContentId="C002";
-  
-    this.scheduleParObj.scheduleParContentList.push(this.objCon2);
+  getFormData() {
+    this.scheduleParObj.id = this.firstFormGroup.value.parId;
+    this.scheduleParObj.empId = this.firstFormGroup.value.empId;
+    this.scheduleParObj.scheduleDate = new Date(this.firstFormGroup.value.parDate);
   }
 
-  checkboxPush(){
-
+  pushEmp() {
+    this.scheduleParObj.id = this.firstFormGroup.value.parId;
+    this.scheduleParObj.empId = this.firstFormGroup.value.empId;
+    this.scheduleParObj.scheduleDate = new Date(this.firstFormGroup.value.parDate);
   }
 
+  contentListPush() {
+    this.scheduleParObj.scheduleParContentList = [];
+    for (let parConfig of this.parConfigArray) {
+      if (parConfig.isActive == true) {
+        this.scheduleParObj.scheduleParContentList.push(new ScheduleParContent(parConfig.id))
+      }
+    }
+  }
+
+  appraisorsListPush() {
+    this.scheduleParObj.scheduleParAppraisorsList = [];
+    for (let parAppraisor of this.parAppraisorArray) {
+      if (parAppraisor.isActive == true) {
+        this.scheduleParObj.scheduleParAppraisorsList.push(new ScheduleParAppraisors(parAppraisor.id))
+      }
+    }
+  }
+
+  schedulePar(){
+  
+    console.log(this.scheduleParObj)
+    this.scheduleParService.addSchedulePar(this.scheduleParObj).subscribe(data=>{
+      alert("ok")
+    })
+  }
 }
