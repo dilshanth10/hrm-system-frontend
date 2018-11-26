63436cf856c5110bfc5931c634681cd02c108983
Inthuja <inthujae@gmail.com>
2018-11-26 12:53:10 +0530

    District,HighestQualification,RecruitmentType UI created

---
+++ src/app/modules/employee/recruitment/view-district/modal/edit-view-district/edit-view-district.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-view-district',
+  templateUrl: './edit-view-district.component.html',
+  styleUrls: ['./edit-view-district.component.css']
+})
+export class EditViewDistrictComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
