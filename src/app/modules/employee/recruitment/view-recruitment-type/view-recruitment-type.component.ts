d5ccdaf1baab7cf1b62f81eb7807d121e31ca8a1
ThamilMoorthy <thamilini94moorthy@gmail.com>
2018-11-26 17:45:04 +0530

    Recruitment-District,HighestQualification,RecruitmentType Get Method done

--- src/app/modules/employee/recruitment/view-recruitment-type/view-recruitment-type.component.ts
+++ src/app/modules/employee/recruitment/view-recruitment-type/view-recruitment-type.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { RecruitmentType } from '../Modal/recruitment-type';
+import { RecruitmentTypeService } from '../Service/recruitment-type.service';
 
 @Component({
   selector: 'app-view-recruitment-type',
@@ -7,31 +9,44 @@ import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
   styleUrls: ['./view-recruitment-type.component.css']
 })
 export class ViewRecruitmentTypeComponent implements OnInit {
+  recruitmentType :RecruitmentType[];
+msg:any;
+
 
   displayedColumns: string[] = ['r_id','r_type','r_button'];
+recruitment:any;
 
-  recruitment = [
-    { 'r_id':'1','r_type':'abc','r_button':' '}
-  
-  ]
+ 
   dataSource = new MatTableDataSource<any>(this.recruitment);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
 
-  constructor() { }
+  constructor(private recruitmentTypeService:RecruitmentTypeService) { }
 
   ngOnInit() {
+    // this.dataSource = new MatTableDataSource<any>(this.recruit);
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+    this.getAllRecruitmentType()
+  }
+
+  // applyFilter(filterValue: string) {
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  //   if (this.dataSource.paginator) {
+  //     this.dataSource.paginator.firstPage();
+  //   }
+  // }
+
+
+  getAllRecruitmentType() {
+    this.recruitmentTypeService.getAllRecruitmentType().subscribe(data => {
+    this.recruitment = data;
     this.dataSource = new MatTableDataSource<any>(this.recruitment);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-  }
-
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
+    console.log(data);
+    });
+}
 }
