b8a924f500b03f3e0febdf133949d779b8cb60ae
paki <tpakikaran14@gmail.com>
2018-12-03 19:27:41 +0530

    validation

--- src/app/modules/general/profiles/add-profile-info/add-general-information/add-general-information.component.ts
+++ src/app/modules/general/profiles/add-profile-info/add-general-information/add-general-information.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProfileInfoService } from '../../view-profile-info/profile-table/profile-info.service';
 import { Profile } from '../../view-profile-info/profile-table/profile.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-general-information',
@@ -16,6 +17,37 @@ export class GeneralInformationComponent implements OnInit {
    
   }
   
+  addUserForm = new FormGroup({
+    userName: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(3)
+    ])),
+    email: new FormControl('', Validators.compose([
+      Validators.required,
+      //Validators.minLength(3),
+      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+    ])),
+    fullName: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(3)
+    ])),
+    employeeName: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(3)
+    ])),
+    nationality: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(3)
+    ])),
+    roleId: new FormControl(''),
+    departmentId: new FormControl(''),
+    date: new FormControl('', Validators.compose([
+      Validators.required,
+      //Validators.minLength(3)
+    ])),
+  });
+
+
 
   addProfileGeneralInfo(){
     return this.profileser.addGeneralInfo(this.proObj).subscribe(data=>{
