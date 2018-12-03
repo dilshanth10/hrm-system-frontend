741a56a84dca784ed23f1d92124b280a83c1a91c
paki <tpakikaran14@gmail.com>
2018-12-03 16:05:44 +0530

    Add profile by user id

--- src/app/modules/general/profiles/add-profile-info/add-academic-qualification/add-academic-qualification.component.ts
+++ src/app/modules/general/profiles/add-profile-info/add-academic-qualification/add-academic-qualification.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AcademicQualificationService } from './academic-qualification.service';
 import { AcademicQualification } from './academic-qualification.model';
+import { Profile } from '../../view-profile-info/profile-table/profile.model';
+import { ProfileInfoService } from '../../view-profile-info/profile-table/profile-info.service';
 
 @Component({
   selector: 'app-add-academic-qualification',
@@ -10,27 +12,49 @@ import { AcademicQualification } from './academic-qualification.model';
 })
 export class AcademicQualificationComponent implements OnInit {
 
-  constructor(private router:Router,private academicService:AcademicQualificationService) { }
   academicObj:AcademicQualification=new AcademicQualification();
+  user:Profile[];
+  constructor(private router:Router,
+    private academicService:AcademicQualificationService,
+    private userService:ProfileInfoService
+    ) { }
  
 
-  previous() {
-    this.router.navigate(['/appointment/appointmentInformation/generalInfo']);
-  }
-
-  next() {
-    this.router.navigate(['/appointment/appointmentInformation/professionalQualification']);
-  }
-
+ 
   ngOnInit() {
-    
+    this.getUserId()
+  }
+  getUserId(){
+    return this.userService.getGenerelInfo().subscribe(data=>{
+      this.user=data;
+    })
   }
     createAcademicQualification(){
       this.academicService.addAcademicQualification(this.academicObj)
       .subscribe(data=>{
-        console.log(data);
-        alert("created");
-        this.next();
+        // console.log(data);
+        // alert("created");
+        // this.next();
+        this.clear();
       })
     }
+
+    previous() {
+      this.router.navigate(['/appointment/appointmentInformation/generalInfo']);
+    }
+  
+    next() {
+      this.router.navigate(['/appointment/appointmentInformation/professionalQualification']);
+    }
+
+    clear() {
+      this.academicObj.examTypeId= null;
+      this.academicObj.periodYearTo = null;
+      this.academicObj.periodYearFrom = null;
+      this.academicObj.result = null;
+      this.academicObj.schoolName = null;
+      this.academicObj.user = null;
+      this.academicObj.examinationYear = null;
+    }
+  
 }
--- src/app/modules/general/profiles/view-profile-info/view-professional-membership/view-professional-membership.ts
+++ src/app/modules/general/profiles/view-profile-info/view-professional-membership/view-professional-membership.ts
@@ -6,7 +6,7 @@ export class ViewProfessionalMembership {
     periodYearTo: Number;
     organizationName: String;
     award: Number;
-    userId: number;
+    user: number;
 
 
 
