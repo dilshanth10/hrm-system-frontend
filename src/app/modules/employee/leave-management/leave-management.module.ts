93c91f3b45f7c7a830d2344a442a22bbb4463172
bihaSobiha <sobiha09@gmail.com>
2018-12-06 22:26:39 +0530

    view specific employees leave request

--- src/app/modules/employee/leave-management/leave-management.module.ts
+++ src/app/modules/employee/leave-management/leave-management.module.ts
@@ -29,6 +29,8 @@ import { CancelLeaveRejectComponent } from './Cancel-Leave/approve-cancel-leave/
 import { ViewLeaveDetailsComponent } from './Cancel-Leave/approve-cancel-leave/Modal/view-leave-details/view-leave-details.component';
 import { RejectedLeaveComponent } from './leave-history/rejected-leave/rejected-leave.component';
 import { AcceptedLeaveComponent } from './leave-history/accepted-leave/accepted-leave.component';
+import { ViewMyLeaveComponent } from './view-my-leave/view-my-leave.component';
+import { ConformCancelLeaveRequestModelComponent } from './view-my-leave/Model/conform-cancel-leave-request-model/conform-cancel-leave-request-model.component';
 
 const routes: Routes = [
   {
@@ -88,7 +90,9 @@ const routes: Routes = [
     ViewLeaveDetailsComponent,
     AllLeaveComponent,
     RejectedLeaveComponent,
-    AcceptedLeaveComponent
+    AcceptedLeaveComponent,
+    ViewMyLeaveComponent,
+    ConformCancelLeaveRequestModelComponent,
   ],
   providers: [LeaveManagementInteractionService]
 })
