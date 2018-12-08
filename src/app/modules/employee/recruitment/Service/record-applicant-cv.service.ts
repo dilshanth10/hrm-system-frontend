35810fe6da4cf62b60fcbff0f2b74bef9b6c9fe9
jana <jjananthan93@gmail.com>
2018-12-08 23:50:23 +0530

    get user by login details

--- src/app/modules/employee/recruitment/Service/record-applicant-cv.service.ts
+++ src/app/modules/employee/recruitment/Service/record-applicant-cv.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { RecordApplicantCv } from '../Modal/record-applicant-cv';
-<<<<<<< Updated upstream
-=======
 // import { ViewRecordApplicantCv } from '../Modal/view-record-applicant-cv';
->>>>>>> Stashed changes
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
