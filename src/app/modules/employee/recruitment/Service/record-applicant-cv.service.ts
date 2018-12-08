907ed45b8d8a77e17044dbb4a1c97f82920357e1
thirupparan <thirupparan1994@gmail.com>
2018-12-08 05:39:44 +0530

    resolve

--- src/app/modules/employee/recruitment/Service/record-applicant-cv.service.ts
+++ src/app/modules/employee/recruitment/Service/record-applicant-cv.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { RecordApplicantCv } from '../Modal/record-applicant-cv';
-<<<<<<< HEAD
 // import { ViewRecordApplicantCv } from '../Modal/view-record-applicant-cv';
-=======
-
->>>>>>> fda0ab5bb87e1728fd27a339dd161564b8496f77
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
