604fede167e6089d69d62dc6f6421ee19af32571
ragulan22 <balasingamragulan@gmail.com>
2019-01-13 11:01:49 +0530

    suggestion and feedback edited

--- src/app/modules/trainer/Service/feedback.service.ts
+++ src/app/modules/trainer/Service/feedback.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Feedback } from '../Model/feedback';
+import { Trainer } from '../Model/trainer';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -10,11 +11,20 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class FeedbackService {
-  private feedbackAPI = "http://localhost:8020/hrm_system/feedback";
+  private feedbackApi = "http://localhost:8020/hrm_system/feedback";
+  private trainerApi = "http://localhost:8020/hrm_system/trainer";
+
 
   constructor(private http: HttpClient) { }
 
   public getFeedback() {
-    return this.http.get<Feedback[]>(this.feedbackAPI);
+    return this.http.get<Feedback[]>(this.feedbackApi);
   }
+  public getTrainer() {
+    return this.http.get<Trainer[]>(this.trainerApi);
+  }
+  public createFeedback(feedback: Feedback) {
+    return this.http.post<Feedback>(this.feedbackApi, feedback)
+  }
+
 }
